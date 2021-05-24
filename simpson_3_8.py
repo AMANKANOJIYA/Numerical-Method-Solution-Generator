6473e34410e0298f89ec83eefe5c6e977ce1f70d
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2021-05-24 23:33:02 +0530

    spelling mistake

--- simpson_3_8.py
+++ simpson_3_8.py
@@ -1,11 +1,11 @@
 x=list(map(float,input("enter a list of y knot value :: ").split(" ")))
 h=eval(input("ENter the gap  :  "))
 
-# simon 3/8
-def simon_b(list_,h):
+# simpson 3/8
+def simpson_b(list_,h):
     formula=((3*h)/8)*((list_[0]+list_[-1])+3*(sum([list_[i] for i in range(1,len(list_)) if (i)%3!=0]))+2*(sum([list_[i] for i in range(2,len(list_)-1,3)])))
     return formula
 
 if __name__=="__main__":
-    z=simon_b(x,h)
-    print(z,"this is your answer from simon 3/8")
+    z=simpson_b(x,h)
+    print(z,"this is your answer from simpson 3/8")
